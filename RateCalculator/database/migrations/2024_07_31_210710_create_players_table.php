76630f758956f8f5c45bfff9479b1acfe09b722e
Inunu1 <inunu1@outlook.com>
2024-07-31 22:05:06 +0900

    マイグレーションファイルとモデルとしーだーつくった

---
+++ RateCalculator/database/migrations/2024_07_31_210710_create_players_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('players', function (Blueprint $table) {
+            //会員id
+            $table->increments('id');
+            //姓
+            $table->string('last_name',20);
+            //名
+            $table->string('first_name',20);
+            //レーティング
+            $table->integer('rating');
+            //レーティング計算フラグ
+            $table->boolean('calcrate_flag')->default(false);
+            //登録日と更新日
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('players');
+    }
+};
---
+++ RateCalculator/database/seeders/PlayersTableSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Player;
+use Illuminate\Support\Facades\DB;
+
+use Carbon\Carbon;
+
+class PlayersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        Player::truncate();
+        
+        $now = Carbon::now();
+        //$formattedDate = $now->format('YmdHis');
+        
+        $lastNames = ['秀生', '智之', '秀介'];
+        $firstNames = ['緒方', '今井', '福井'];
+        $rates = array(2500, 3000, 2000);
+        $calcrateFlags = [false, false, false];
+        
+        for ($i = 1; $i <= 3; $i++) {
+            DB::table('players')->insert([
+                'id' => $i,
+                'last_name' => $lastNames[$i-1],
+                'first_name' => $firstNames[$i-1],
+                'rating' => $rates[$i-1],
+                'calcrate_flag' => $calcrateFlags[$i-1],
+                'created_at' => $now,
+                'updated_at' => $now,
+            ]);
+        }
+    }
+}
